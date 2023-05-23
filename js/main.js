72937e696069f8614824e217b99be4ab6dc18d05
Michele de Vita <116875835+maykmayk@users.noreply.github.com>
2023-05-23 17:08:16 +0200

    web-mid

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    console.log("owl init")
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+    items: 6,
+    loop:true,
+    margin: 10,
+    dots: false,
+    autoplay:true,
+    stagePadding: 10,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive: {
+        0: {
+            items: 2,
+        },
+        415: {
+            items: 3,
+        },
+        768: {
+            items: 4,
+        },
+        992: {
+            items:6,
+        },
+
+    }
+});
+});
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
