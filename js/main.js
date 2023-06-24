c381200539710b2e8cad3727762f330e243432e2
maykmayk <m.devita@ied.edu>
2023-06-24 10:55:29 +0200

    owl fixed

--- js/main.js
+++ js/main.js
@@ -1,24 +1,62 @@
-function openNav() {
+const _app = {}
+
+_app.openNav = () => {
     document.getElementById("myNav").style.width = "100%";
-  }
+}
   
-function closeNav() {
+_app.closeNav = () => {
     document.getElementById("myNav").style.width = "0%";
-  }
+}
 
-  var current = 0;
-  var icons = ['../asset/images/favicon/favicon1.ico', '../asset/images/favicon/favicon2.ico', '../asset/images/favicon/favicon3.ico', '../asset/images/favicon/favicon4.ico'];
-  // Every 2 seconds, switch your icon
-  setInterval(function () {
-      // Determine the next icon
-      var icon = (++current % icons.length);
-      // Grab the URL to use
-      var url = icons[icon];
-      // Update your elements
-      document.getElementById('icon-a').href = url;
-      document.getElementById('icon-b').href = url;
-  }, 2000);
+_app.favicon = () => {
+    var current = 0;
+    var icons = ['../asset/images/favicon/favicon1.ico', '../asset/images/favicon/favicon2.ico', '../asset/images/favicon/favicon3.ico', '../asset/images/favicon/favicon4.ico'];
+    // Every 2 seconds, switch your icon
+    setInterval(function () {
+        // Determine the next icon
+        var icon = (++current % icons.length);
+        // Grab the URL to use
+        var url = icons[icon];
+        // Update your elements
+        document.getElementById('icon-a').href = url;
+        document.getElementById('icon-b').href = url;
+    }, 2000);
+}
 
-  const clip = document.querySelectorAll(".hover-to-play");
-  for (let i = 0; i < clip.length; i++) { clip[i].addEventListener("mouseenter", function (e) { clip[i].play();
-    }); clip[i].addEventListener("mouseout", function (e) { clip[i].pause(); }); }
+_app.owlCarousel = () => {
+  $(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      lazyLoad:false,
+      dots: false,
+      stagePadding: 20,
+      loop:true,
+      margin:10,
+      autoplay:true,
+      autoplayTimeout:2500,
+      autoplayHoverPause:true,
+      responsive:{
+        0:{
+            items:3,
+        },
+        600:{
+            items:4,
+        },
+        1000:{
+            items:5,
+        }
+      }
+    });
+  });
+}
+
+_app.startUp = () => {
+    _app.openNav();
+    _app.closeNav();
+    _app.favicon();
+    _app.owlCarousel();
+}
+
+_app.startUp();
+  
+
+  
