039b18dfec24855275d13d2f329550afd5e9d704
maykmayk <m.devita@ied.edu>
2023-09-09 23:21:38 +0200

    --

--- js/main.js
+++ js/main.js
@@ -75,110 +75,111 @@ _app.textAnim = () => {
 	tl.from(".line span", 1.8, {
 		y: 100,
 		ease: "power4.out",
-		delay: .5,
+		delay: 0.5,
 		skewY: 7,
 		stagger: {
 			amount: 0.3
 		}
 	});
-	
 }
 
-_app.loader = () => {
-	return new Promise((resolve) => {
-		_app.body.classList.add("overflow-hidden");
-		const tl = gsap.timeline();
-		const welcomeScreen = gsap.timeline({
-		  paused: true,
-		});
+// _app.loader = () => {
+// 	return new Promise((resolve) => {
+// 		_app.body.classList.add("overflow-hidden");
+// 		const tl = gsap.timeline();
+// 		const welcomeScreen = gsap.timeline({
+// 		  paused: true,
+// 		});
 		
-		tl.from(".title", {
-			duration: 0.5,
-			opacity: 0,
-			y: 10,
-		});
-		tl.from(".bracket", {
-			duration: 0.3,
-			scale: 0,
-			margin: 0,
-		});
-		tl.from("#loader", {
-			duration: 0.2,
-			scale: 0,
-		});
-		tl.from("loading-images, img", {
-			duration: 0.8,
-			y: 150,
-			opacity: 0,
-			stagger: {
-				amount: 01,
-			},
-		});
-		tl.from(
-			".bottom-line",
-			{
-				duration: 0.5,
-				y: 50,
-				opacity: 0,
-				stagger: {
-				amount: 0.1,
-				},
-			},
-			"-=.5"
-		);
-		welcomeScreen.to(".loading-section, .loading-images-container", {
-			y: -10,
-			opacity: 0,
-		});
-		welcomeScreen.to(".loading-screen", {
-			duration: 0.8,
-			y: -2000,
-			ease: "Power4.out",
-		});
-		welcomeScreen.from(".welcome-screen", {
-				y: 200,
-				duration: 0.5,
-				stagger: {
-				amount: 0.2,
-				},
-			}, "-=.8"
-		);
-		welcomeScreen.eventCallback("onComplete", () => {
-			resolve();
-		  });
-		let id,
-		i = 0;
-		function loader() {
-		id = setInterval(frame, 45);
-		}
-		function frame() {
-		if (i >= 100) {
-			clearInterval(id);
-			welcomeScreen.play();
-		} else {
-			i++;
-			document.getElementById("loader").innerHTML = i + "%";
-		}
-		}
-		window.onload = function () {
-		loader();
-		};
-	});
-}
+// 		tl.from(".title", {
+// 			duration: 0.5,
+// 			opacity: 0,
+// 			y: 10,
+// 		});
+// 		tl.from(".bracket", {
+// 			duration: 0.3,
+// 			scale: 0,
+// 			margin: 0,
+// 		});
+// 		tl.from("#loader", {
+// 			duration: 0.2,
+// 			scale: 0,
+// 		});
+// 		tl.from("loading-images, img", {
+// 			duration: 0.8,
+// 			y: 150,
+// 			opacity: 0,
+// 			stagger: {
+// 				amount: 01,
+// 			},
+// 		});
+// 		tl.from(
+// 			".bottom-line",
+// 			{
+// 				duration: 0.5,
+// 				y: 50,
+// 				opacity: 0,
+// 				stagger: {
+// 				amount: 0.1,
+// 				},
+// 			},
+// 			"-=.5"
+// 		);
+// 		welcomeScreen.to(".loading-section, .loading-images-container", {
+// 			y: -10,
+// 			opacity: 0,
+// 		});
+// 		welcomeScreen.to(".loading-screen", {
+// 			duration: 0.8,
+// 			y: -2000,
+// 			ease: "Power4.out",
+// 		});
+// 		welcomeScreen.from(".welcome-screen", {
+// 				y: 200,
+// 				duration: 0.5,
+// 				stagger: {
+// 				amount: 0.2,
+// 				},
+// 			}, "-=.8"
+// 		);
+// 		welcomeScreen.eventCallback("onComplete", () => {
+// 			resolve();
+// 		  });
+// 		let id,
+// 		i = 0;
+// 		function loader() {
+// 		id = setInterval(frame, 45);
+// 		}
+// 		function frame() {
+// 		if (i >= 100) {
+// 			clearInterval(id);
+// 			welcomeScreen.play();
+// 		} else {
+// 			i++;
+// 			document.getElementById("loader").innerHTML = i + "%";
+// 		}
+// 		}
+// 		window.onload = function () {
+// 		loader();
+// 		};
+// 	});
+// }
 
 _app.startUp = () => {
 	if (window.location.pathname.includes("index.html")) {
 		_app.owlCarousel();
-		_app.loader().then(() => {
-			_app.body.classList.remove("overflow-hidden");
-			setTimeout(() => {				
-				document.getElementById("text-open").classList.remove("opacity-0", "invisible");
-			}, 100);
-			_app.textAnim();
-			_app.menuGestor();
-		});	
+		// _app.loader().then(() => {
+		// 	_app.body.classList.remove("overflow-hidden");
+		// 	setTimeout(() => {				
+		// 		document.getElementById("text-open").classList.remove("opacity-0", "invisible");
+		// 	}, 100);
+		// 	_app.textAnim();
+		// 	_app.menuGestor();
+		// });	
+		_app.textAnim();
+		_app.menuGestor();
 	}
-
+	
 	_app.menuGestor();
 
 	if (window.location.pathname.includes("works.html")) {
