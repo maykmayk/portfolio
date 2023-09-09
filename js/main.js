4a72792809cd7331bf6b503eada16ca1b44b35eb
maykmayk <m.devita@ied.edu>
2023-09-09 22:49:38 +0200

    ---

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const _app = {}
+_app.body = document.querySelector("body")
 
 _app.owlCarousel = () => {
 	$(document).ready(function(){
@@ -44,52 +45,6 @@ _app.owlCarousel = () => {
 	});
 }
 
-_app.pageReveal = () => {
-	
-	function startLoader() {
-		_app.counterElement = document.querySelector(".counter");
-		let currentValue = 0;
-		let targetValue = 100;
-		let increment = 1;
-	  
-		function updateCounter() {
-		  if (currentValue >= targetValue) {
-			return;
-		  }
-	  
-		  currentValue += increment;
-		  _app.counterElement.textContent = currentValue;
-	  
-		  requestAnimationFrame(updateCounter);
-		}
-	  
-		updateCounter();
-	  }
-	  
-	  startLoader();
-	  
-
-	gsap.to(".counter", 0.25, {
-		delay: 2.5,
-		opacity: 0,
-	})
-
-	gsap.to(".bar", 0.25, {
-		delay: 2.5,
-		height: 0,
-		stagger: {
-			amount: 0.5,
-		},
-		ease: "power4.inOut",
-	})
-
-	setTimeout(() => {
-		_app.bar = document.querySelector(".overlay")
-		_app.counterElement.classList.add("d-none")
-		_app.bar.classList.add("d-none")
-	}, 3500);
-}
-
 _app.smooth = () => {
 	const lenis = new Lenis({
 		direction: 'vertical',
@@ -117,34 +72,127 @@ _app.smooth = () => {
 _app.textAnim = () => {
 	const tl = gsap.timeline();
 
-	// Prima animazione (.line span)
 	tl.from(".line span", 1.8, {
-	y: 100,
-	ease: "power4.out",
-	delay: 3,
-	skewY: 7,
-	stagger: {
-		amount: 0.3
-	}
+		y: 100,
+		ease: "power4.out",
+		delay: .5,
+		skewY: 7,
+		stagger: {
+			amount: 0.3
+		}
+	});
+	
+}
+
+_app.loader = () => {
+	return new Promise((resolve) => {
+		_app.body.classList.add("overflow-hidden");
+		const tl = gsap.timeline();
+		const welcomeScreen = gsap.timeline({
+		  paused: true,
+		});
+		
+		tl.from(".title", {
+			duration: 0.5,
+			opacity: 0,
+			y: 10,
+		});
+		tl.from(".bracket", {
+			duration: 0.3,
+			scale: 0,
+			margin: 0,
+		});
+		tl.from("#loader", {
+			duration: 0.2,
+			scale: 0,
+		});
+		tl.from("loading-images, img", {
+			duration: 0.8,
+			y: 150,
+			opacity: 0,
+			stagger: {
+				amount: 01,
+			},
+		});
+		tl.from(
+			".bottom-line",
+			{
+				duration: 0.5,
+				y: 50,
+				opacity: 0,
+				stagger: {
+				amount: 0.1,
+				},
+			},
+			"-=.5"
+		);
+		welcomeScreen.to(".loading-section, .loading-images-container", {
+			y: -10,
+			opacity: 0,
+		});
+		welcomeScreen.to(".loading-screen", {
+			duration: 0.8,
+			y: -2000,
+			ease: "Power4.out",
+		});
+		welcomeScreen.from(".welcome-screen", {
+				y: 200,
+				duration: 0.5,
+				stagger: {
+				amount: 0.2,
+				},
+			}, "-=.8"
+		);
+		welcomeScreen.eventCallback("onComplete", () => {
+			resolve();
+		  });
+		let id,
+		i = 0;
+		function loader() {
+		id = setInterval(frame, 45);
+		}
+		function frame() {
+		if (i >= 100) {
+			clearInterval(id);
+			welcomeScreen.play();
+		} else {
+			i++;
+			document.getElementById("loader").innerHTML = i + "%";
+		}
+		}
+		window.onload = function () {
+		loader();
+		};
 	});
 }
 
 _app.startUp = () => {
-	_app.owlCarousel();
-	_app.menuGestor();
-	_app.smooth();
-	_app.pageReveal();
-	_app.textAnim();
+	if (window.location.pathname.includes("index.html")) {
+		_app.owlCarousel();
+		_app.loader().then(() => {
+			_app.body.classList.remove("overflow-hidden");
+			setTimeout(() => {				
+				document.getElementById("text-open").classList.remove("opacity-0", "invisible");
+			}, 100);
+			_app.textAnim();
+			_app.menuGestor();
+		});	
+	}
 
-	var workCont = document.querySelectorAll(".workImg");
-	workCont.forEach(work => {
-		work.addEventListener("mouseover", function(e) {
-			_app.startFollowing();
-		})
-		work.addEventListener("mouseleave", function(e) {
-			_app.leaveFollowing();
-		})
-	});
+	_app.menuGestor();
+
+	if (window.location.pathname.includes("works.html")) {
+		var workCont = document.querySelectorAll(".workImg");
+		workCont.forEach(work => {
+			work.addEventListener("mouseover", function(e) {
+				_app.startFollowing();
+			})
+			work.addEventListener("mouseleave", function(e) {
+				_app.leaveFollowing();
+			})
+		});
+	}
+	_app.smooth();
 }
 
 _app.startFollowing = () => {
@@ -171,7 +219,8 @@ _app.menuGestor = () =>{
 	_app.navEl = document.querySelector("#navbar");
 	setTimeout(() => {
 		_app.navEl.classList.add("fixed-top")
-	}, 3000);
+	}, 6000);
+
 	_app.prevScroll = window.scrollY;
 	document.addEventListener("wheel", (e) => {
 	if (e.deltaY > 0) {
